806a146aa9b0ddf1a6d1763ab4ce91d5a98f23f0
amanpalap <pal90589500@gmail.com>
2024-03-12 22:56:41 +0530

    A blogging website

---
+++ src/store/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    status : false,
+    userDate : null
+}
+
+const authSlice = createSlice({
+    name: "auth",
+    initialState,
+    reducer: {
+        login: (state, action)=>{
+            state.status = true
+            state.userData = action.payload.userDate
+        },
+        logout: (state)=>{
+            state.status = false
+            state.userData = null
+        },
+
+    }
+}) 
+
+export const {login, logout} = authSlice.actions
+
+export default authSlice.reducer
